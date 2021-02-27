6da5825439b1626e3f7714aa465df5abf83bff40
Blessin Varkey <blessinvarkey@gmail.com>
2021-02-27 20:57:41 +0530

    Update layout.py

--- Fundamentals/layout.py
+++ Fundamentals/layout.py
@@ -2,3 +2,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+# Import files from sklearn to load data and to test/train the model
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+
+# Import Sagemaker files for AWS
+import os
+import sagemaker import get_execution_role
+from sagemaker.amazon.amazon_estimator import get_image_uri
+
